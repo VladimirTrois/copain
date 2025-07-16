39a74ea662a8ab28b863b88d397010e96a8f77ee
vladimir <vladimir.trois@gmail.com>
2025-07-16 16:28:05 +0200

    Add exceptions handled by listener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 
 #[AsEventListener]
 class ExceptionListener
@@ -31,6 +33,21 @@ class ExceptionListener
             $responseData = [
                 'error' => 'Validation failed',
                 'details' => json_decode($exception->getMessage(), true),
+            ];
+        }
+
+        if ($exception instanceof NotEncodableValueException) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $responseData = [
+                'error' => 'Invalid JSON',
+                'details' => $exception->getMessage(),
+            ];
+        }
+        if ($exception instanceof ExceptionInterface) {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $responseData = [
+                'error' => 'Deserialization error',
+                'details' => $exception->getMessage(),
             ];
         }
 
